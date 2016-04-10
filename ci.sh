a93b3e73782c0eb4a9298f8ce50a611d4934afce
Jorge Aparicio <japaricious@gmail.com>
2016-04-10 10:50:17 -0500

    protect deploy-* branches from divergence

--- ci.sh
+++ ci.sh
@@ -3,11 +3,15 @@
 set -ex
 
 main() {
-    if [ "$TRAVIS_BRANCH" = "deploy-$IMAGE" ] && [ "$TRAVIS_PULL_REQUEST" == false ]; then
-        set +x
-        docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
-        set -x
-        bash $IMAGE/deploy.sh
+    if [ "$TRAVIS_BRANCH" = "deploy-$IMAGE" ] && ; then
+        if [ "$TRAVIS_PULL_REQUEST" == false ]; then
+            set +x
+            docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+            set -x
+            bash $IMAGE/deploy.sh
+        else
+            echo 'error: deploy branches must not diverge from master' && exit 1
+        fi
     else
         bash $IMAGE/deploy.sh --test-only
     fi
